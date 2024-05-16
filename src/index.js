1df4765385b84d2743476bcb603e9c20d98c00fd
henkdebeunhaas <thomaskroezen@outlook.com>
2024-05-16 11:20:23 +0200

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 const port = 3000;
-let list = [];
+const cats = [
+    {
+        name : "henk",
+        age : "69",
+        gender : "yes"
+    },
+    {
+        name : "piet",
+        age : "420",
+        gender : "no"
+    }
+]
 
-app.get('/beunhaas', function (req, res) {
-    res.send('beunhazen');
+app.get('/cats', function (req, res) {
+    res.status(200);
+    res.json(cats);
 });
 
 app.get('/', function (req, res) {
@@ -13,7 +26,8 @@ app.get('/', function (req, res) {
 });
 
 app.post('/makestuff', function (req, res) {
-
+    const text = req.body;
+    res.send("Wollah " + text.name);
 });
 
 app.put('/', function (req, res) {
