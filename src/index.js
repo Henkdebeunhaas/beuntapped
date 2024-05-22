5e47e2390fd4322d747ad7d44505731e9cc15d75
henkdebeunhaas <thomaskroezen@outlook.com>
2024-05-22 14:01:16 +0200

    Added some more request with more options

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const statusCodes = require('http-status-codes');
 
 app.use(express.json());
 const port = 3000;
@@ -16,18 +17,29 @@ const cats = [
     }
 ]
 
-app.get('/cats', function (req, res) {
-    res.status(200);
-    res.json(cats);
+app.get('/greet/:name', function (req, res) {
+    res.status(statusCodes.OK)
+    const lang = req.query.lang;
+    if(lang == 'nl'){
+        res.send(`Hallo ${req.params.name} ${JSON.stringify(req.body)}`);
+    } else if(lang == 'en'){
+        res.send("Hello " + req.params.name);
+    }
+
+    if(req.body){
+        res.send(req.body);
+    }
+
 });
 
 app.get('/', function (req, res) {
     res.send('je moeder');
 });
 
-app.post('/makestuff', function (req, res) {
-    const text = req.body;
-    res.send("Wollah " + text.name);
+app.post('/cats', function (req, res) {
+    const cat = req.body
+    cats.push(cat);
+    res.send("Wollah " + cat.name);
 });
 
 app.put('/', function (req, res) {
@@ -36,6 +48,13 @@ app.put('/', function (req, res) {
 
 app.delete('/', function (req, res) {
 
+});
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+        .json({
+            message: err.message || "Something went wrong.",
+    });
 })
 
 app.listen(port, function (){
